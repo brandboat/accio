d667639c60fd685ea90a487fc4b68d3e53de88d6
brandboat <brandboat@gmail.com>
2023-10-12 18:07:04 +0800

    Remove

--- accio-sqlrewrite/src/main/java/io/accio/sqlrewrite/analyzer/ExpressionAnalyzer.java
+++ accio-sqlrewrite/src/main/java/io/accio/sqlrewrite/analyzer/ExpressionAnalyzer.java
@@ -14,18 +14,26 @@
 
 package io.accio.sqlrewrite.analyzer;
 
-import com.google.common.collect.Lists;
 import io.accio.base.AccioMDL;
+import io.accio.base.dto.Column;
+import io.accio.base.dto.Model;
 import io.accio.base.dto.Relationship;
 import io.trino.sql.tree.DefaultTraversalVisitor;
 import io.trino.sql.tree.DereferenceExpression;
 import io.trino.sql.tree.Expression;
 import io.trino.sql.tree.Identifier;
+import io.trino.sql.tree.QualifiedName;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Set;
 
+import static io.accio.base.AccioMDL.getRelationshipColumn;
+import static io.accio.base.dto.JoinType.reverse;
+import static io.trino.sql.tree.DereferenceExpression.getQualifiedName;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public final class ExpressionAnalyzer
@@ -57,6 +65,7 @@ public final class ExpressionAnalyzer
     {
         private final AccioMDL accioMDL;
         private final Scope scope;
+        private final Model model = null;
 
         public Visitor(AccioMDL accioMDL, Scope scope)
         {
@@ -67,30 +76,65 @@ public final class ExpressionAnalyzer
         @Override
         protected Void visitDereferenceExpression(DereferenceExpression node, Void ignored)
         {
+//            LinkedList<Identifier> identifiers = getIdentifiers(node);
+//
+//            Identifier identifier = popQuietly(identifiers);
+//            if (identifier != null && identifier.getValue().equals(model.getName())) {
+//                identifier = identifiers.pop();
+//            }
+//
+//            while (identifier != null) {
+//                getColumn(model, identifier.getValue())
+//                        .flatMap(Column::getRelationship)
+//                        .ifPresent(relationshipName ->
+//                                relationships.add(
+//                                        accioMDL.getRelationship(relationshipName)
+//                                                .orElseThrow(() -> new NoSuchElementException(format("relationship %s not found", relationshipName)))));
+//                identifier = identifiers.pop();
+//            }
+
+            QualifiedName qualifiedName = getQualifiedName(node);
+            if (qualifiedName != null) {
+
+            }
+
+            // make sure all relationships are defined and valid in current expression
             return null;
         }
     }
 
-    private static LinkedList<Expression> elements(Expression expression)
+    // collect all identifiers in DereferenceExpression, if any expression in DereferenceExpression
+    // is not identifier, return empty list.
+    // e.g. expression is a.b.c then return List.of(a, b, c)
+    //      expression is a.b[0].c then return List.of()
+    private static LinkedList<Identifier> getIdentifiers(DereferenceExpression expression)
     {
         Expression current = expression;
-        LinkedList<Expression> elements = new LinkedList<>();
+        LinkedList<Identifier> elements = new LinkedList<>();
         while (true) {
-            if (current instanceof Identifier) {
-                elements.add(current);
-                // in dereference expression, function call or identifier should be the root node
-                break;
-            }
-            else if (current instanceof DereferenceExpression) {
-                elements.add(current);
+            if (current instanceof DereferenceExpression) {
+                ((DereferenceExpression) current).getField().ifPresent(elements::addFirst);
                 current = ((DereferenceExpression) current).getBase();
             }
-            else {
-                // unexpected node in dereference expression, clear everything and return
-                elements.clear();
+            else if (current instanceof Identifier) {
+                elements.addFirst((Identifier) current);
                 break;
             }
+            else {
+                elements.clear();
+                return new LinkedList<>();
+            }
         }
-        return new LinkedList<>(Lists.reverse(elements));
+        return elements;
+    }
+
+    private static <T> T popQuietly(LinkedList<T> list)
+    {
+        try {
+            return list.pop();
+        }
+        catch (NoSuchElementException ex) {
+            return null;
+        }
     }
 }
